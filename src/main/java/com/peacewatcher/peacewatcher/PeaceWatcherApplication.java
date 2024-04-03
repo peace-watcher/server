9ee6062a3e8e2444184a21d230bbf409b17b4ea6
ri-naa <rina288@ewhain.net>
2024-04-03 20:41:18 +0900

    initial commit

---
+++ src/main/java/com/peacewatcher/peacewatcher/PeaceWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.peacewatcher.peacewatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaceWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaceWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peacewatcher/peacewatcher/PeaceWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peacewatcher.peacewatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaceWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
