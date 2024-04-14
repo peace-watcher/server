8eb352e7992919dd87f550c605e38ff69614bfc7
ri-naa <rina288@ewhain.net>
2024-04-14 18:20:10 +0900

    [fix] cors credentials 설정 수정

--- src/main/java/com/peacewatcher/peacewatcher/Config/WebConfig.java
+++ src/main/java/com/peacewatcher/peacewatcher/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")  // 허용할 오리진
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메소드
-                .allowCredentials(true)  // 쿠키를 포함할지 여부
+                .allowCredentials(false)  // 쿠키를 포함할지 여부
                 .maxAge(3600);  // pre-flight 요청의 결과를 캐시할 시간(초 단위)
     }
 }
